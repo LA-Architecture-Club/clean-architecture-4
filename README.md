a7532a7b23908afa05fdcc7d130dea5ad105f5d4
Michael McCormick <mmccormick@pivotal.io>
2015-08-05 00:07:28 -0700

    Initial Commit
    Gets a basic Spring boot project together and includes a PivotalTracker project export for a quick start to working on the Clean architecture project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Clean Architecture
+
+## Setup
+ 1. Read [the Clean Architecture](https://blog.8thlight.com/uncle-bob/2012/08/13/the-clean-architecture.html)
+ 2. Import into [PivotalTracker](https://pivotaltracker.com) l_a__architecture_club___ci_template_20150805_0622.csv
---
+++ src/main/java/LACleanArchitecture/LaacApplication.java
@@ -0,0 +1,12 @@
+package LACleanArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaacApplication.class, args);
+    }
+}
